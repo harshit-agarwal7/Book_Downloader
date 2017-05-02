f6ad7591d6e0605df061f403af998f21bd842572
whitechain <harsit.agarwal_ug19@ashoka.edu.in>
2017-05-03 03:35:02 +0530

    Add files via upload

---
+++ Book_Downloader.py
@@ -0,0 +1,68 @@
+import urllib
+import urllib2
+from bs4 import BeautifulSoup
+
+def DownloadURL(Contents):          #Function to extract the URL of that page where the download link is present
+    url, extension = '', ''         
+    flaglang, flagext, flag = 0, 0, 0       
+    for results in range(len(Contents)):         #Iterates through every element of the Contents list     
+        for i in Contents[results]:         #Iterates through every line of each element of the Contents list
+            links = str(i).split('"')       #Extracts web links from strings
+            for lines in i:
+                if 'English' in lines:      #Checks if the language of the book is English
+                    flaglang = 1
+                if 'pdf' in lines:      #Checks if the book exists in PDF format
+                    flagext = 1
+                    extension = '.pdf'
+                elif 'epub' in lines:       
+                    flagext = 1
+                    extension = '.epub'
+                elif 'mobi' in lines:       
+                    flagext = 1
+                    extension = '.mobi'
+            flag = flaglang * flagext       #Checks if both conditions are true
+            if flag == 1:
+                for lines in links:         #Iterates through links
+                    if 'ads' in lines:      #Checks if the substring 'ads' appears in links, which is the link of the webpage where the download link is present
+                        url = 'http://libgen.io' + lines        #Creates a libgen download page link
+                        break
+        if flag == 1:break
+
+    return url, extension       #Returns the download page URL and file extension
+
+def Download(url, title, extension):       #Function to download the book
+    page = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(page, 'lxml')
+    links = soup.find_all('a')             #Finds all the links in the webpage
+    for link in links:
+        if 'get.php' in str(link):            
+            temp = str(link).split('"')    #Stores all the links with get.php in them
+    dloadurl = 'http://libgen.io' + temp[1]      #Generates the download link. 
+    print 'Downloading book...'
+    dloadurl = dloadurl.replace('amp;','')       
+    title = title.replace(' ','_')
+    location = 'C:\Users\MY\Desktop\%s' %(title+extension)        #Stores the file in this location. The location needs to be changed when run on a different system.
+    urllib.urlretrieve(dloadurl, location)
+    print 'Download Completed!'
+
+title, author = '', ''      
+title = raw_input('Please enter the name of the book: ')        
+author = raw_input('Please enter the name of the author: ')        
+title_author = title + ' ' + author        #A single search is created which can be used to search the book
+url = 'http://libgen.io/search.php?req='
+url += urllib.quote_plus(title_author)        #Creates a search URL for Libgen
+page = urllib2.urlopen(url).read()      #Opens the search URL
+print "Searching for book..."
+if 'view.php?id' not in page:        #Checks if there are any downlaod links on the page
+    print 'Book does not exist.'
+
+else:
+    print "Book exists."
+    soup = BeautifulSoup(page, 'lxml')      #Creates objects of every element of the webpage using the library 'Beautiful Soup'
+    Contents = []    
+    for sibling in soup.find("table",{"class":"c"}).tr.next_siblings:       #Extracts contents of a table with class 'c' from the HTML code of the webpage
+        Contents.append(sibling)        #Adds the contents to the Contents list
+    url, extension = DownloadURL(Contents)
+    Download(url, title, extension)
+
+
