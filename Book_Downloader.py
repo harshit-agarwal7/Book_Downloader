56a23c48308f057c946b47d0492c6580acc70f8c
Navdeep Singh <navdeepwrites@gmail.com>
2017-05-04 22:34:48 +0530

    Update Book_Downloader.py

--- Book_Downloader.py
+++ Book_Downloader.py
@@ -2,6 +2,29 @@ import urllib
 import urllib2
 from bs4 import BeautifulSoup
 
+def Search(title, author):      #Function to search for the book    
+    while len(title) == 0:
+        title = raw_input('Please enter the name of the book: ')        
+    while len(author) == 0:
+        author = raw_input('Please enter the name of the author: ')
+    print "Searching for book..."
+    title_author = title + ' ' + author        #Creates a single search term which is used to search the book
+    url = 'http://libgen.io/search.php?req='
+    url += urllib.quote_plus(title_author)        #Creates a search URL for Libgen
+    page = urllib2.urlopen(url).read()      #Opens the search URL
+
+    if 'view.php?id' not in page:        #Checks if there are any results on the page
+        print 'Book not found.'
+
+    else:
+        print "Book found."
+        soup = BeautifulSoup(page, "html.parser")      #Creates objects of every element of the webpage using the library 'Beautiful Soup'
+        Contents = []    
+        for sibling in soup.find("table",{"class":"c"}).tr.next_siblings:       #Extracts contents of a table with class 'c' from the HTML code of the webpage
+            Contents.append(sibling)        #Adds every row of the table to the Contents list
+        url, extension = DownloadURL(Contents)
+        Download(url, title, extension)
+
 def DownloadURL(Contents):          #Function to extract the URL of the page that contains the download URL
     url, extension = '', ''         
     flaglang, flagext, flag = 0, 0, 0       
@@ -45,27 +68,6 @@ def Download(url, title, extension):       #Function to download the book
     urllib.urlretrieve(dloadurl, location)
     print 'Download Completed!'
 
-title, author = '', ''      
-while len(title) == 0:
-    title = raw_input('Please enter the name of the book: ')        
-while len(author) == 0:
-    author = raw_input('Please enter the name of the author: ')
-print "Searching for book..."
-title_author = title + ' ' + author        #Creates a single search term which is used to search the book
-url = 'http://libgen.io/search.php?req='
-url += urllib.quote_plus(title_author)        #Creates a search URL for Libgen
-page = urllib2.urlopen(url).read()      #Opens the search URL
 
-if 'view.php?id' not in page:        #Checks if there are any results on the page
-    print 'Book not found.'
-
-else:
-    print "Book found."
-    soup = BeautifulSoup(page, "html.parser")      #Creates objects of every element of the webpage using the library 'Beautiful Soup'
-    Contents = []    
-    for sibling in soup.find("table",{"class":"c"}).tr.next_siblings:       #Extracts contents of a table with class 'c' from the HTML code of the webpage
-        Contents.append(sibling)        #Adds every row of the table to the Contents list
-    url, extension = DownloadURL(Contents)
-    Download(url, title, extension)
-
-
+title, author = '', ''
+Search(title,author)
